e6719e8ddb5c13e72c64abe63425121e64d10d12
VinnCihuyy <cihuyyvinzz@gmail.com>
2025-10-28 11:34:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tombol sapa = document.getElementBYId("sapa Button");
+const sapa saya = document.getElementBYId("sapa Button");
 tombolsapa.addEventLIstener('click', function() {
   alert('halo!thx udh ngunjungin web gw jan lupa share meme ya' )
 });
